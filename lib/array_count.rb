7a5ca5ba8c6cb0bd7c917603cdc89202e1f21da4
Marty Warfel <marthawarfel@gmail.com>
2020-11-30 01:45:49 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,19 @@
-def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
-end
+require "pry"
 
+def count_strings(array)
+ 
+  array.count do |element|
+    element.class == String 
+  end
+   
+end 
+ 
+  
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+  array.count do |element, index|
+  element.class == String
+  array.index.even?
+      binding.pry
+  end
+
 end
