ab2bdadb17c6cb2ca78c574e0e823eb7d78f00e3
Marty Warfel <marthawarfel@gmail.com>
2020-11-30 01:46:07 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -11,9 +11,8 @@ end
   
 def count_empty_strings(array)
   array.count do |element, index|
-  element.class == String
-  array.index.even?
-      binding.pry
+  element.class.even? == String
+  
   end
 
 end
